94382fedc59e219e87f71fe8337f1f882229bf6e
留白 <wyqpadding@gmail.com>
2017-11-02 23:20:42 +0800

    0.0.2

---
+++ YQAnswerButtonFramework/YQAnswerButtonFramework.h
@@ -0,0 +1,19 @@
+//
+//  YQAnswerButtonFramework.h
+//  YQAnswerButtonFramework
+//
+//  Created by Wang on 2017/11/2.
+//  Copyright © 2017年 Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YQAnswerButtonFramework.
+FOUNDATION_EXPORT double YQAnswerButtonFrameworkVersionNumber;
+
+//! Project version string for YQAnswerButtonFramework.
+FOUNDATION_EXPORT const unsigned char YQAnswerButtonFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YQAnswerButtonFramework/PublicHeader.h>
+
+
